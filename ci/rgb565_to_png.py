614d5cd2c62c06923633747447bb0eba5d24db5d
Corran Webster <cwebster@unital.dev>
2024-10-18 11:42:47 +0100

    Add a basic byte-checked test for examples.

---
+++ ci/rgb565_to_png.py
@@ -0,0 +1,38 @@
+import array
+from pathlib import Path
+
+from PIL import Image
+
+def read_rgb565(path):
+    with open(path, 'rb') as fp:
+        buffer = fp.read()
+
+    rgb565 = array.array('H', buffer)
+    rgb565.byteswap()
+    return rgb565
+
+def rgb565_to_rgb24(rgb565):
+    rgb24 = array.array('B', bytearray(3*len(rgb565)))
+    for i, pixel in enumerate(rgb565):
+        r = (pixel & 0b1111100000000000) >> 11
+        g = (pixel & 0b0000011111100000) >> 5
+        b = (pixel & 0b0000000000011111)
+        rgb24[3 * i] = int((r / 31) * 255)
+        rgb24[3 * i + 1] = int(round((g / 63) * 255))
+        rgb24[3 * i + 2] = int(round((b / 31) * 255))
+    return rgb24
+
+
+def rgb565_to_png(size, in_path, out_path=None):
+    if out_path is None:
+        out_path = Path(in_path.stem + '.png')
+
+    rgb565 = read_rgb565(in_path)
+    rgb24 = rgb565_to_rgb24(rgb565)
+    image = Image.frombuffer("RGB", size, rgb24)
+    image.save(out_path)
+
+if __name__ == "__main__":
+    import sys
+
+    rgb565_to_png((320, 240), Path(sys.argv[1]))
---
+++ tests/tempe/test_examples.py
@@ -0,0 +1,60 @@
+# SPDX-FileCopyrightText: 2024-present Unital Software <info@unital.dev>
+#
+# SPDX-License-Identifier: MIT
+
+import array
+import unittest
+
+from tempe.display import FileDisplay
+
+working_buffer = array.array('H', bytearray(320*61*2))
+
+class TestExamples(unittest.TestCase):
+
+    def test_examples(self):
+        """Bytewise comparison that output of examples is what is expected."""
+
+        examples = [
+            ("examples/shapes_examples.py", "tests/tempe/shapes.rgb565"),
+        ]
+        for file, result in examples:
+            print(file, result)
+            self.subTest(example=file)
+
+            code = open(file, 'r').read()
+            locals = {"__name__": "__test__"}
+            exec(code, locals)
+
+            output = self.display_output(locals["surface"])
+
+            self.assert_files_equal(output, result)
+
+    def display_output(self, surface, name='example.rgb565'):
+        display = FileDisplay(name, (320, 240))
+
+        with display:
+            display.clear()
+            surface.refresh(display, working_buffer)
+
+        return name
+
+    def assert_files_equal(self, file1, file2):
+        with open(file1, 'rb') as f1:
+            with open(file2, 'rb') as f2:
+                i = 0
+                while True:
+                    b1 = f1.read(1)
+                    b2 = f2.read(1)
+                    if b1 != b2:
+                        print(f"Byte {i}")
+                    self.assertEqual(b1, b2)
+                    if not b1:
+                        break
+                    i += 1
+
+
+if __name__ == "__main__":
+    result = unittest.main()
+    if result.errors or result.failures:
+        import sys
+        sys.exit(1)
